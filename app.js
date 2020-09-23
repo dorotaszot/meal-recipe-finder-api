990c4bee73c8ee712bd65bdc838a77593a6da55d
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2020-09-23 16:42:46 +0200

    changed from object classes to one js file

--- app.js
+++ app.js
@@ -1,19 +1,54 @@
-const meals = new Meals();
+// const meals = new Meals();
 
 const mealSearch = document.getElementById('meal-search');
 const searchBtn = document.getElementById('search-btn');
 const randomSearchBtn = document.getElementById('random-search-btn');
-
-
-
-
+const resultHeading = document.getElementById('result-heading');
+const mealsResults = document.getElementById('meals-results');
+const singleMealResult = document.getElementById('single-meal');
 
 function getMeals(e) {
   e.preventDefault();
   const term = mealSearch.value;
-  meals.getMeals(term);
-
-
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}
+    `)
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data);
+        if (data.meals === null) {
+          resultHeading.innerHTML = `<h3 class="meals-results">There are no results for'${term}'</h3>`
+          mealsResults.innerHTML = '';
+        } else {
+          resultHeading.innerHTML = `<h3 class="meals-results">Search results for '${term}':</h3>`;
+          mealsResults.innerHTML = data.meals.map(meal =>
+            `
+            <div class="meal-img-box">
+              <img src="${meal.strMealThumb}" class='meal-img'>
+              <div class="meal-info" data-mealID='${meal.idMeal}'>
+                <h3>${meal.strMeal}</h3>
+              </div>
+            </div>
+            `
+          ).join('')
+        }
+      });
+    mealSearch.value = '';
+  } else {
+    resultHeading.innerHTML = `<h3 class="meals-results">Please enter search term</h3>`;
+  }
 };
 
+function showSingleMeal(e) {
+  // const mealID;
+
+  if (e.target.classList.contains('meal-img') || e.target.classList.contains('meal-info')) {
+    console.log('ok');
+  } else {
+    console.log('no');
+  }
+}
+
 searchBtn.addEventListener('click', getMeals);
+mealsResults.addEventListener('click', showSingleMeal);
+
--- meals.js
+++ meals.js
@@ -1,39 +1,40 @@
-class Meals {
-  constructor(term) {
-    this.term = term;
-  }
+// class Meals {
+//   constructor(term) {
+//     this.term = term;
+//   }
 
-  getMeals(term) {
-    const resultHeading = document.getElementById('result-heading');
-    const mealsResults = document.getElementById('meals-results');
-    const singleMealResult = document.getElementById('single-meal');
-    const mealSearch = document.getElementById('meal-search');
+//   getMeals(term) {
+//     const resultHeading = document.getElementById('result-heading');
+//     const mealsResults = document.getElementById('meals-results');
+//     const singleMealResult = document.getElementById('single-meal');
+//     const mealSearch = document.getElementById('meal-search');
 
-    if (term.trim()) {
-      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}
-      `)
-        .then(res => res.json())
-        .then(data => {
-          console.log(data);
-          if (data.meals === null) {
-            resultHeading.innerHTML = `<h3 class="meals-results">There are no results for'${term}'</h3>`
-          } else {
-            resultHeading.innerHTML = `<h3 class="meals-results">Search results for '${term}':</h3>`;
-            mealsResults.innerHTML = data.meals.map(meal =>
-              `
-              <div class="meal-img-box">
-                <img src="${meal.strMealThumb}" class='meal-img'>
-                <div class="meal-info" data-mealID='${meal.idMeal}'>
-                  <h3>${meal.strMeal}</h3>
-                </div>
-              </div>
-              `
-            ).join('')
-          }
-        });
-      mealSearch.value = '';
-    } else {
-      resultHeading.innerHTML = `<h3 class="meals-results">Please enter search term</h3>`;
-    }
-  }
-}
+//     if (term.trim()) {
+//       fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}
+//       `)
+//         .then(res => res.json())
+//         .then(data => {
+//           // console.log(data);
+//           if (data.meals === null) {
+//             resultHeading.innerHTML = `<h3 class="meals-results">There are no results for'${term}'</h3>`
+//             mealsResults.innerHTML = '';
+//           } else {
+//             resultHeading.innerHTML = `<h3 class="meals-results">Search results for '${term}':</h3>`;
+//             mealsResults.innerHTML = data.meals.map(meal =>
+//               `
+//               <div class="meal-img-box">
+//                 <img src="${meal.strMealThumb}" class='meal-img'>
+//                 <div class="meal-info" id='${meal.idMeal}'>
+//                   <h3>${meal.strMeal}</h3>
+//                 </div>
+//               </div>
+//               `
+//             ).join('')
+//           }
+//         });
+//       mealSearch.value = '';
+//     } else {
+//       resultHeading.innerHTML = `<h3 class="meals-results">Please enter search term</h3>`;
+//     }
+//   }
+// }
