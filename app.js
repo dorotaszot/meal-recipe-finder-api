c7010f014bc5b489619bcd276f3b3d039c2a4e55
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2020-09-24 08:44:38 +0200

    show single deal started

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ function getMeals(e) {
             <div class="meal-img-box">
               <img src="${meal.strMealThumb}" class='meal-img'>
               <div class="meal-info" data-mealID='${meal.idMeal}'>
-                <h3>${meal.strMeal}</h3>
+                <h3 class="meal-info-text">${meal.strMeal}</h3>
               </div>
             </div>
             `
@@ -39,16 +39,44 @@ function getMeals(e) {
   }
 };
 
-function showSingleMeal(e) {
-  // const mealID;
+function getSingleMeal(mealID) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}
+  `)
+    .then(res => res.json())
+    .then(data => {
+      const mealDetails = data.meals[0];
+      console.log(mealDetails);
+      showSingleMealDetails(mealDetails);
+    })
+}
 
-  if (e.target.classList.contains('meal-img') || e.target.classList.contains('meal-info')) {
-    console.log('ok');
-  } else {
-    console.log('no');
+function showSingleMealDetails(mealDetails) {
+  singleMealResult.innerHTML = `
+  <h2 class="center mtb">${mealDetails.strMeal}</h2>
+  <img src="${mealDetails.strMealThumb}">
+  <p class="mtb">${mealDetails.strInstructions}</p>
+  <h3 class="mtb center">Ingredients:</h3>
+
+  `
+}
+
+function showSingleMeal(e) {
+  const mealInfo = e.path.find(item => {
+    if (item.classList) {
+      return item.classList.contains('meal-info');
+    } else {
+      return false;
+    }
+  })
+  // console.log(mealInfo);
+
+  if (mealInfo) {
+    const mealID = mealInfo.getAttribute('data-mealID');
+    getSingleMeal(mealID);
   }
 }
 
+// Event listeners
 searchBtn.addEventListener('click', getMeals);
 mealsResults.addEventListener('click', showSingleMeal);
 
